babb547a734a6784f76624f908b1493452bc29cb
Brian Chan <brian.chan@liferay.com>
2024-04-19 09:05:37 +0100

    LPD-22247 Sublime sort

--- modules/dxp/apps/osb/osb-faro/osb-faro-docker/cloud/100_liferay_image_setup.sh
+++ modules/dxp/apps/osb/osb-faro/osb-faro-docker/cloud/100_liferay_image_setup.sh
@@ -11,6 +11,6 @@ cp -R /home/liferay/resources/system-ext.properties ${LIFERAY_HOME}/tomcat/webap
 rm ${LIFERAY_HOME}/deploy/trial-dxp-license-*.xml
 
 sed -i '/<!-- APR library loader/i \ <Listener className=\"org.apache.catalina.security.SecurityListener\" checkedOsUsers=\"root\" minimumUmask=\"0007\" \/>\n' $LIFERAY_HOME/tomcat/conf/server.xml
-sed -i s/"<Listener className=\"org.apache.catalina.core.AprLifecycleListener\" SSLEngine=\"on\" \/>"/"<Listener className=\"org.apache.catalina.core.AprLifecycleListener\" SSLEngine=\"off\" \/>"/ $LIFERAY_HOME/tomcat/conf/server.xml
-sed -i '/<\/Host>/i \ <Valve className=\"org.apache.catalina.valves.RemoteIpValve\" remoteIpHeader=\"X-Forwarded-For\" protocolHeader=\"X-Forwarded-Proto\" \/>' $LIFERAY_HOME/tomcat/conf/server.xml
 sed -i '/<Connector port=\"8080\"/,/\/>/c \<Connector\n acceptorThreadCount=\"2\"\n bindOnInit=\"false\"\n connectionTimeout=\"20000\"\n enableLookups=\"false\"\n maxHttpHeaderSize=\"65536\"\n maxPostSize=\"4194304\"\n maxThreads=\"200\"\n minSpareThreads=\"50\"\n port=\"8080\"\n protocol=\"org.apache.coyote.http11.Http11Nio2Protocol\"\n server=\"\"\n URIEncoding=\"UTF-8\"\n \/>\n' $LIFERAY_HOME/tomcat/conf/server.xml
+sed -i '/<\/Host>/i \ <Valve className=\"org.apache.catalina.valves.RemoteIpValve\" remoteIpHeader=\"X-Forwarded-For\" protocolHeader=\"X-Forwarded-Proto\" \/>' $LIFERAY_HOME/tomcat/conf/server.xml
+sed -i s/"<Listener className=\"org.apache.catalina.core.AprLifecycleListener\" SSLEngine=\"on\" \/>"/"<Listener className=\"org.apache.catalina.core.AprLifecycleListener\" SSLEngine=\"off\" \/>"/ $LIFERAY_HOME/tomcat/conf/server.xml
